de1bb85c2b03ce79bbd6d59a93cce2caef102321
adykarta <mkartadibrata@gmail.com>
2019-10-20 09:10:47 +0700

    Updates

---
+++ precache-manifest.c49c8815d0c62c46ec97d1bd216d62d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65079b8eabafe56821adad921894019a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0318802b8bb382248b50",
+    "url": "/portfolio/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "bbb03aae6b4b7946f117",
+    "url": "/portfolio/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "0318802b8bb382248b50",
+    "url": "/portfolio/static/js/2.38ad7dcb.chunk.js"
+  },
+  {
+    "revision": "bbb03aae6b4b7946f117",
+    "url": "/portfolio/static/js/main.0aa0f6a8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
